d500fc68b4345d5f88a1f53f9e5b512a5284ffd7
rodolforomera <rodolforomeras@gmail.com>
2023-03-01 10:07:16 -0300

    Added product use cases

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -2,4 +2,8 @@ export default interface ProductInterface {
     readonly id: string;
     readonly name: string;
     readonly price: number;
+    
+    changeName(name: string): void;
+    changePrice(price: number): void;
+    validate(): boolean;
 }
